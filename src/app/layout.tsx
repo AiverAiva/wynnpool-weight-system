42aa5fc3c7d321747cbb459e711ca535d38f9311
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2025-05-05 00:57:51 +0800

    chore: update google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics Script */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-43G08YVE2R"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-43G08YVE2R');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
