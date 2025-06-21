4aee02a103546287ec122b4c19be624fbe1decf5
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2025-06-21 14:50:40 +0800

    chore: add uuid field in item document

--- src/app/api/verified-items/route.ts
+++ src/app/api/verified-items/route.ts
@@ -22,6 +22,23 @@ export async function POST(req: Request) {
         return new Response(JSON.stringify({ error: "Missing required fields." }), { status: 400 });
     }
 
+    // If owner is a name, fetch UUID from Mojang API
+    if (typeof body.owner === "string") {
+        //  && !body.owner.match(/^[0-9a-fA-F]{32}$/)
+        try {
+            const resp = await fetch(`https://api.mojang.com/users/profiles/minecraft/${body.owner}`);
+            if (resp.ok) {
+                const data = await resp.json();
+                if (data && data.id) {
+                    body.owner = data.name; 
+                    body.uuid = data.id
+                }
+            }
+        } catch (e) {
+            return new Response(JSON.stringify({ error: "unknown owner" }), { status: 400 });
+        }
+    }
+
     await db.insertOne(body);
     return new Response(JSON.stringify({ success: true }), { status: 200 });
 }
